11ddf694635a776a75883707aa45dbf0675d6dd2
am-an04 <aman3135942@gmail.com>
2026-03-15 23:53:08 +0530

    debugging

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,21 +1,29 @@
-import {Router} from 'express';
-import { registerUser } from "../controllers/user.controller.js"
-import upload from "../middlewares/multer.middleware.js"
+import { Router } from "express";
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router =Router()
+
+
+const router = Router()
+
 router.route("/register").post(
     upload.fields([
-         {
-           names:"avatar",
-           maxCount:1
-         },
-         {
-          name:"coverImage",
-          maxCount:1
-         }
+        {
+            name: "avatar",
+            maxCount: 1
+        }, 
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
     ]),
-    registerUser)
+    registerUser
+    )
 
-    router.route("/login").post(loginUser)
-    router.route("/login").post(verifyJWT,logoutUser)
-export default router
+router.route("/login").post(loginUser)
+
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
+
+export default router;
